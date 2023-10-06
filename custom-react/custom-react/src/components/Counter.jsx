3f4431953a3cc6c1400b5e93c6c2332934cb93d9
= <=>
2023-10-06 21:43:42 +0530

    changes

--- custom-react/custom-react/src/components/Counter.jsx
+++ custom-react/custom-react/src/components/Counter.jsx
@@ -3,25 +3,41 @@ import { useState } from 'react'
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
+    const [limit, setLimit] = useState('');
     function increaseCounter() {
-        setCounter(counter + 1)
+        if (counter >= 10) {
+            setLimit(`Maximum Limit Reached at : ${counter}`)
+        } else {
+            setCounter(counter + 1);
+            setLimit('')
+        }
     }
     function decreaseCounter() {
-        setCounter(counter - 1)
+        if (counter <= 0) {
+            setLimit(`Minimum Limit Reached at : ${counter}`)
+        } else {
+            setCounter(counter - 1);
+            setLimit('')
+        }
     }
     function refreshWindow() {
         // if counter is not 0 , it will not refresh
-        if (counter != 0) {
+        if (counter !== 0) {
             window.location.reload();
         }
     }
+
     return (
         <>
+            <h1 style={{ textAlign: 'center' }}>Counter App</h1>
             <div className="main">
+                <button onClick={decreaseCounter}>-</button>
                 <h1 id='display'>{counter}</h1>
                 <button onClick={increaseCounter}>+</button>
-                <button onClick={decreaseCounter}>-</button>
                 <button id='refreshBtn' onClick={refreshWindow}>RESET</button>
+            </div>
+            <div className="limit">
+                <h2 style={{ textAlign: 'center' }}>{limit}</h2>
             </div>
 
         </>
