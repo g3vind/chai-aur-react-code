6ef1eba8696d785e7ca4e1973f51389fc80cbb12
= <=>
2023-10-06 22:35:34 +0530

    tailwind css and props

---
+++ props-and-tailwindcss/src/App.jsx
@@ -0,0 +1,19 @@
+import { Card } from "./components/Card"
+
+function App() {
+  return (
+
+    <>
+      <div className="flex justify-center items-center h-screen p-4">
+        <Card title="Macbook" img="https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDYyOTJ8MHwxfHNlYXJjaHwyfHxsYXB0b3BzfGVufDB8fHx8MTY5NjYxMTMzNXww&ixlib=rb-4.0.3&q=80&w=400" />
+        <Card title="Nacbook" img="https://images.unsplash.com/photo-1603302576837-37561b2e2302?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDYyOTJ8MHwxfHNlYXJjaHw0fHxsYXB0b3BzfGVufDB8fHx8MTY5NjYxMTMzNXww&ixlib=rb-4.0.3&q=80&w=400" />
+        <Card title="Pacbook" img="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDYyOTJ8MHwxfHNlYXJjaHw1fHxsYXB0b3BzfGVufDB8fHx8MTY5NjYxMTMzNXww&ixlib=rb-4.0.3&q=80&w=400" />
+        <Card title="Racbook" img="https://images.unsplash.com/photo-1611078489935-0cb964de46d6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDYyOTJ8MHwxfHNlYXJjaHwxfHxsYXB0b3BzfGVufDB8fHx8MTY5NjYxMTMzNXww&ixlib=rb-4.0.3&q=80&w=400" />
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
